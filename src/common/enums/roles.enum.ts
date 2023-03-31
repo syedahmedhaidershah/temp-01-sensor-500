e49f690bea1db1fc043a86698ae58c37cfa2eca0
MalharAli <malhar.ali@by433.com>
2023-03-31 17:00:34 +0500

    roles added, status and guards implemented on Beach chair state endpoints

--- src/common/enums/roles.enum.ts
+++ src/common/enums/roles.enum.ts
@@ -2,4 +2,7 @@ export enum Role {
   SuperAdmin = 'superadmin',
   Admin = 'admin',
   User = 'user',
+  SerivceAccount = 'service-account',
+  Vacationer = 'vacationer',
+  TechnicianAccount = 'technician-account'
 }
--- src/modules/chair/chair.controller.ts
+++ src/modules/chair/chair.controller.ts
@@ -1,21 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, HttpCode, HttpStatus } from '@nestjs/common';
 import { PaginationDefaultQuery } from 'src/common/interfaces';
 import { ChairService } from './chair.service';
 import { CreateChairDto } from './dto/create-chair.dto';
 import { UpdateChairDto } from './dto/update-chair.dto';
 import { UpdateChairStateDto } from './dto/update-chair-state.dto';
 import { ChairType } from './types';
-import { ChairStates } from 'src/common/enums';
+import { ChairStates, Role } from 'src/common/enums';
 import { Chair } from 'src/database/mongoose/schemas';
+import { Authorization } from 'src/common/decorators';
 
+@Authorization(Role.Admin)
 @Controller('chair')
 export class ChairController {
   constructor(private readonly chairService: ChairService) { }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createChairDto: CreateChairDto): Promise<ChairType> {
     return this.chairService.create(createChairDto);
   }
+
 
   @Get()
   findAll(
@@ -44,32 +48,44 @@ export class ChairController {
     return this.chairService.findById(id);
   }
 
+  @Authorization(Role.SerivceAccount)
   @Get('update-chair-state/online/:id')
+  @HttpCode(HttpStatus.OK)
   updateChairStateToOnline(@Param('id') id: string): Promise<Chair> {
     return this.chairService.updateChairStateToOnline(id);
   }
 
+  @Authorization(Role.Admin,Role.TechnicianAccount)
   @Get('update-chair-state/offline/:id')
+  @HttpCode(HttpStatus.OK)
   updateChairStateToOffline(@Param('id') id: string): Promise<Chair> {
     return this.chairService.updateChairStateToOffline(id);
   }
 
+  @Authorization(Role.Vacationer,Role.Admin)
   @Get('update-chair-state/reserved/:id')
+  @HttpCode(HttpStatus.OK)
   updateChairStateToReserved(@Param('id') id: string): Promise<Chair> {
     return this.chairService.updateChairStateToReserved(id);
   }
 
+  @Authorization(Role.Vacationer)
   @Get('update-chair-state/opened/:id')
+  @HttpCode(HttpStatus.OK)
   updateChairStateToOpened(@Param('id') id: string): Promise<Chair> {
     return this.chairService.updateChairStateToOpened(id);
   }
 
+  @Authorization(Role.SerivceAccount)
   @Get('update-chair-state/defective/:id')
+  @HttpCode(HttpStatus.OK)
   updateChairStateToDefect(@Param('id') id: string): Promise<Chair> {
     return this.chairService.updateChairStateToDefect(id);
   }
 
+  @Authorization(Role.Vacationer)
   @Get('update-chair-state/temporarily-defective/:id')
+  @HttpCode(HttpStatus.OK)
   updateChairStateToTemporarilyDefective(@Param('id') id: string): Promise<Chair> {
     return this.chairService.updateChairStateToTemporarilyDefective(id);
   }
