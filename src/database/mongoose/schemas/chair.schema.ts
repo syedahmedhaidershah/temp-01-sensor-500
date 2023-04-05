0cb8f2ae9a63b4fb2b30347ff1066d4bb177ac2c
ahmedhaider-cp <ahmed.haider@cloudprimero.com>
2023-04-05 15:06:18 +0500

    INTERIM | docs: notes on chair + reservations

--- src/database/mongoose/schemas/chair.schema.ts
+++ src/database/mongoose/schemas/chair.schema.ts
@@ -16,6 +16,19 @@ export class Chair {
 
   @Prop({ required: true })
   qr_code?: string;
+
+  /**
+   * @note GREEN-18 - As a vacationer, I want to scan a QR code at a reserved beach chair so that I can easily
+   * find a nearby free beach chair that meets my selected filtering criteria, such as location, availability, number of
+   * seats, and the presence of USB-charging stations.
+   * @note DB Principles used: Eventual Consistency, Basically available, Redundancy (Works fine in NoSQL Document DBs) 
+   * @todo 
+   * 1. Insert reservations array within beach chair, implement FIFO
+   * 2. Inquire Product for a limitation of maximum reservations
+   * 3. Create a Reservations collection, and drop the same redundant data in two places:
+   *    a. The reservations array prop in chair as a nested document
+   *    b. Reservations collection as a document
+   */
 }
 
 export const ChairSchema = SchemaFactory.createForClass(Chair);
