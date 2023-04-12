6909d132ba96a2ce9053cb86ad9f8ff2e36a6b0d
darijalicp <darij.ali@cloudprimero.com>
2023-04-12 13:11:57 +0500

    GREEN-48-refactor-constants-decoupling

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -1,3 +1,5 @@
+import { ErrorMessagesConstants } from './error-messages.constant';
+
 export const Constants = {
   JwtStrategyConstants: {
     JWT: 'jwt',
@@ -11,15 +13,7 @@ export const Constants = {
     COMMON_USERS: ['user'],
   },
   ErrorMessages: {
-    ACCESS_DENIED: 'Access Denied',
-    USER_USERNAME_ALREADY_EXIST: 'User with this username already exists',
-    ADMIN_USERNAME_ALREADY_EXIST: 'Admin with this username already exists',
-    INVALID_EMAIL: 'Invalid email address',
-    OTP_EXPIRED: 'OTP Expired',
-    INCORRECT_OTP: 'Incorrect OTP provided. Please input correct OTP.',
-    NO_USER_FOUND: 'No user found.',
-    CHAIR_UPDATE_DENIED: 'This action can`t be performed for this chair state',
-    EMAIL_NOT_FOUND: 'Email not found',
+    ...ErrorMessagesConstants,
   },
   OmitProperties: {
     PASSWORD: 'password',
