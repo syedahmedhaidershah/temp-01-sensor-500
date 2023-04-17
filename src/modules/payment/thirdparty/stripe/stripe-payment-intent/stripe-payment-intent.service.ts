03a962d85692c9a60f4478ac49644d1ae4f11975
ahmedhaider-cp <ahmed.haider@cloudprimero.com>
2023-04-17 12:14:29 +0500

    GRX-55 | feat:  stripe - payment intents basic

---
+++ src/modules/payment/thirdparty/stripe/stripe-payment-intent/stripe-payment-intent.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { StripeService } from '../stripe.service';
+import { ConfirmPaymentData, CreatePaymentIntentData, AttachMethodCustomerData } from '../types';
+import { Constants } from 'src/common/constants';
+import { ResourceLockedException } from 'src/common/exceptions';
+
+@Injectable()
+export class StripePaymentIntentService extends StripeService {
+
+    async createPaymentIntent(
+        data: CreatePaymentIntentData,
+    ) {
+        const {
+            user,
+            payment,
+        } = data;
+
+        const getIntent = this.cache.get(user._id);
+
+        if (getIntent)
+            throw new ResourceLockedException(Constants.ErrorMessages.PAYMENT_ALREADY_INPROGRESS);
+
+        const {
+            payment_method,
+            confirmation_method,
+        } = payment;
+
+        const paymentIntent = await this.instance.paymentIntents.create(payment);
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const cached = await this.cache.set(
+            user._id
+                .concat(payment_method)
+                .concat(confirmation_method),
+            paymentIntent
+        )
+
+        return paymentIntent;
+    }
+
+    async attachMethodToCustomer(
+        data: AttachMethodCustomerData
+    ) {
+        const {
+            customerId: customer,
+            paymentMethod: { id: paymentMethodId }
+        } = data;
+
+        const paymentMethodAttached = await this.instance.paymentMethods
+            .attach(
+                paymentMethodId,
+                {
+                    customer,
+                });
+
+        return paymentMethodAttached;
+    }
+
+    async confirmPayment(
+        data: ConfirmPaymentData,
+    ) {
+        const {
+            paymentIntentId,
+            paymentMethod
+        } = data;
+
+        const intent = await this.instance.paymentIntents
+            .confirm(
+                paymentIntentId, {
+                payment_method: paymentMethod,
+            });
+
+        return intent;
+    }
+}
